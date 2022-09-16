32b752a748abb4076638fe714ea89240b62332b8
Lily Miller <lmille10@uoregon.edu>
2022-09-16 16:33:26 -0700

    new features

--- testgui1.py
+++ testgui1.py
@@ -1,7 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QLabel, QLineEdit, QToolBar, QMainWindow, QAction, QFileDialog, QGridLayout
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QLabel, QLineEdit, QToolBar, QMainWindow, QAction, QFileDialog, QGridLayout, QCheckBox, QHBoxLayout, QFormLayout, QTableView
 import PyQt5.QtCore as QtCore
 import PyQt5.QtWidgets as QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
 import tkinter as tk
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib
@@ -13,15 +15,36 @@ import pandas as pd
 # show what file is loaded
 # print that with jpeg option
 # tic add up all values in second column of original csvs then divide by integration if whatever mass you want
-#display color bar
 # x1 = 76  # len1_len2_peaklist.csv
 # y1 = 521
 x1 = 4 # len1_len2_peaklist.csv
 y1 = 4
+class TableModel(QtCore.QAbstractTableModel): #I dont know i just stole all this code
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self._data[0])
+
 class Window(QDialog,QMainWindow):
     def __init__(self, parent=None):
         self.temp = 0
         self.templist = []
+        self.array = []
         super(Window, self).__init__(parent)
         # a figure instance to plot on
         self.figure = plt.figure()
@@ -36,25 +59,46 @@ class Window(QDialog,QMainWindow):
         self.toolbar.addSeparator()
         # mass search textbox
         self.label = QLabel(self)
+        self.checkbox = QCheckBox("Normalize Data")
         self.line = QLineEdit("Enter Mass")
-        self.line.setFixedSize(100,25)
+        self.line.setFixedSize(300,25)
+        self.line1 = QLineEdit("Enter Color Scalar")
+        self.line1.setFixedSize(200,25)
+        self.label = QLabel("           ")
+        self.checkbox1 = QCheckBox("Apply Color Scalar")
+        self.table = QTableView()
         # Just some button connected to `heatmap` method
         self.button = QPushButton('Plot')
+        #vspacer = QtWidgets.QSpacerItem(500, 40)
         self.button.clicked.connect(self.heatmap)
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(self.line)
+        hlayout.addWidget(self.checkbox)
+        hlayout1 = QHBoxLayout()
+        hlayout1.addWidget(self.checkbox1)
+        hlayout1.addWidget(self.line1)
+        hlayout1.addWidget(self.label)
         # set the layout
         layout = QGridLayout()
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
         layout.addWidget(self.button)
-        layout.addWidget(self.line,3,0)
+        layout.addLayout(hlayout,3,0)
+        layout.addLayout(hlayout1,4,0)
+        layout.addWidget(self.table,5,0)
         self.setLayout(layout)
-    def heatarrayg(self):
-        if self.temp != 0:
+    def table(self):
+        data = [[4,3,2],[5,2,3]] #table
+        self.model = TableModel(data)
+        self.table.setModel(self.model)
+        self.table.show()
+    def heatarrayg(self): #this function would be faster if all csvs were combined into one and pd was used but i dont feel like it
+        if self.temp != 0: #resetting our class variable
             self.temp = 0
-        loadlst = Window.loadlist(self)
+        loadlst = Window.loadlist(self)  #getting our list of files to load
         lengthload = len(loadlst)
         list = []
-        try:
+        try: #loading our files
             for i in range(0, lengthload):
                 a = 0
                 a = np.genfromtxt(loadlst[i], delimiter=',',
@@ -66,7 +110,8 @@ class Window(QDialog,QMainWindow):
         except:
             print("error!")
         self.temp= list
-    def loadlist(self):
+
+    def loadlist(self): #list of files to load (what a mess)
         if self.templist !=0:
             self.templist = []
         path1 = QFileDialog.getExistingDirectory(self) + '/'
@@ -78,20 +123,23 @@ class Window(QDialog,QMainWindow):
                 path = path1 + pathnum
                 self.templist.append(path)
         return(self.templist)
-    def heatarray(self):
-        lengthload = len(Window.loadlist(self))
-        loadlst = Window.loadlist(self)
-        list = []
-        for i in range(0, lengthload):
-            a = pd.read_csv(loadlst[i], delimiter=',', dtype=float) #this is slow, but so is every other method. Not sure if it can be optimized.
-            a.to_numpy()
-            a = np.delete(a, 0, 0) #removes first row
-            a = a[:-1] #removes last row
-            a = np.delete(a, [3, 2], 1) #removes last two columns
-            list.append(a)
-        return(list)
-    def arraymanipulation(self,x1,y1):
+
+    # def heatarray(self):
+    #     lengthload = len(Window.loadlist(self))
+    #     loadlst = Window.loadlist(self)
+    #     list = []
+    #     for i in range(0, lengthload):
+    #         a = pd.read_csv(loadlst[i], delimiter=',', dtype=float) #this is slow, but so is every other method. Not sure if it can be optimized.
+    #         a.to_numpy()
+    #         a = np.delete(a, 0, 0) #removes first row
+    #         a = a[:-1] #removes last row
+    #         a = np.delete(a, [3, 2], 1) #removes last two columns
+    #         list.append(a)
+    #     return(list)
+
+    def arraymanipulation(self,x1,y1): #gives us our final array
         arr = self.temp
+        self.array = arr[0]
         lengthload = len(arr)
         heatarray = np.empty((0), float)
         val1 = int(self.line.text())
@@ -108,17 +156,28 @@ class Window(QDialog,QMainWindow):
         heatarray1 = np.reshape(heatarray, (x1, y1)) #reshapes the empty array into the size we want
         return heatarray1
 
-    def heatmap(self):
+    def heatmap(self): #draw that shit
         array = Window.arraymanipulation(self, x1, y1)
+        if self.checkbox.isChecked() ==True:
+            array = array/np.linalg.norm(array)
         self.figure.clear()
         ax = self.figure.add_subplot(111)
         ax.set_axis_on()
         array = array * (array >= 0)# if less than 0 make it 0
         plt.axis('off')
-        ax.imshow(array,cmap='hot',interpolation='none')
+        if self.checkbox1.isChecked() == True:
+            quantile = np.quantile(array, 0.99)
+            quantile = quantile * float(self.line1.text())
+            im = ax.imshow(array,cmap='hot',interpolation='none',vmax=quantile)
+        if self.checkbox1.isChecked() ==False:
+            im = ax.imshow(array, cmap='hot', interpolation='none')
         ax.set_aspect(5)
-        ax.color
+        #divider = make_axes_locatable(ax)
+        #cax = divider.append_axes("right",size="5%",pad=0.05)
+        cax = self.figure.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height]) #somehow makes my color bar axis the same height as my heatmap
+        plt.colorbar(im,cax=cax) #colorbar
         self.canvas.draw()
+        Window.table(self)
         # fig = plt.figure()
         # ax = fig.add_subplot(111)
         # ax = plt.Axes(fig, [0., 0., 1., 1.])
